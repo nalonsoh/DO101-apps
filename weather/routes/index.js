9f3f47e316f81234ffaf605ca74e8513e4f69edb
Noel Alonso Hernández <noelah@ext.inditex.com>
2020-01-22 09:41:13 +0100

    more logs added

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -17,6 +17,8 @@ router.post('/get_weather', async function (req,res) {
   try {
     let data = await fetch(url);
     let weather = await data.json();
+    console.log("UNITS: " + process.env.UNITS);
+    console.log("URL: " + url);
     console.log(weather);
     if(weather.cod == '404' && weather.main == undefined) {
       res.render('index', {weather: null, error: 'Error: Unknown city'});
