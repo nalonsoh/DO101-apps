930d84be0d9cf8a25f3bf496b23845b741d01dba
Noel Alonso Hernández <noelah@ext.inditex.com>
2020-02-18 17:35:51 +0100

    reset

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,4 +1,4 @@
 <?php
-  print "This is v2 and I am running on host ->  <br>Hola CAF" . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "This is v1 and I am running on host ->" . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
  
 ?>
